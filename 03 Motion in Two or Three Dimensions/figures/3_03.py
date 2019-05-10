443fa4aa5bf67d38632acbed279900facd2400a8
Eric Nguyen <33969138+airicbear@users.noreply.github.com>
2019-05-10 12:17:50 -0400

    :pencil2: Fix 3.3 plot

--- 03 Motion in Two or Three Dimensions/figures/3_03.py
+++ 03 Motion in Two or Three Dimensions/figures/3_03.py
@@ -1,14 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-soa = np.array([[0, 0, 4, 0], [4, 0, 14, 10], [0, 0, 18, 10]])
-X, Y, U, V = zip(*soa)
+rx = lambda t: (4.0 + (2.5) * t ** 2)
+ry = lambda t: 5.0 * t
+
+vx = lambda t: 5.0 * t
+vy = lambda t: 5.0 + t * 0
+
+x = np.linspace(0, 5, 100)
+
+rvec = np.array([[0, 0, rx(0), ry(0)], [0, 0, rx(2), ry(2)]])
+vvec = np.array([[rx(0), ry(0), vx(0), vy(0)], [rx(2), ry(2), vx(2), vy(2)]])
+rX, rY, rZ, rV = zip(*rvec)
+vX, vY, vZ, vV = zip(*vvec)
 plt.figure()
 ax = plt.gca()
-ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, color=['r','b','black'])
-ax.set_xlim([-1, 20])
+ax.quiver(rX, rY, rZ, rV, angles='xy', scale_units='xy', scale=1, color='b')
+ax.quiver(vX, vY, vZ, vV, angles='xy', scale_units='xy', scale=1, color='g')
+ax.set_xlim([-1, 30])
 ax.set_ylim([-1, 20])
 plt.draw()
+plt.plot(rx(x), ry(x), linestyle='--', lw=2)
+plt.scatter([rx(0), rx(2)], [ry(0), ry(2)], color='black')
+
+for vec in rvec:
+    plt.annotate(r'$\vec r\langle{}, {}\rangle$'.format(vec[2], vec[3]), [vec[2], vec[3]])
+
+for i in range(0, len(vvec)):
+    plt.annotate(r'$\vec v\langle{}, {}\rangle$'.format(vvec[i][2], vvec[i][3]), [rvec[i][2] + vvec[i][2], rvec[i][3] + vvec[i][3]])
 
 plt.savefig('3_03')
 plt.show()
